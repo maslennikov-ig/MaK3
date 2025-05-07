cba9ea5d9f8a6d971b2c1887d9d878a1db3452b7
Your Name <your.email@example.com>
2025-05-07 10:58:47 +0300

    chore: добавлены линтеры и форматтеры кода (ESLint, Prettier, Stylelint, EditorConfig)

---
+++ .stylelintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+  ],
+  plugins: [
+    'stylelint-order',
+  ],
+  rules: {
+    'order/properties-alphabetical-order': true,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+          'layer',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+  },
+};
